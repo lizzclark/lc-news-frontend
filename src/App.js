a1d20570191eb2b228c992228917d81b70d54954
lizzclark <erclark13@outlook.com>
2019-02-18 12:51:12 +0000

    create top-level components Header, Footer, Nav, Main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Components/Header.jsx';
+import Nav from './Components/Nav.jsx';
+import Main from './Components/Main.jsx';
+import Footer from './Components/Footer.jsx';
 
 class App extends Component {
   render() {
-    return <div className="App">App</div>;
+    return (
+      <div className="App">
+        <Header />
+        <Nav />
+        <Main />
+        <Footer />
+      </div>
+    );
   }
 }
 

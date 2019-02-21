a73971fe95995f628003c9af7f2f519060854510
lizzclark <erclark13@outlook.com>
2019-02-21 10:09:15 +0000

    link avatars as well as usernames on linked UserCard

--- src/Components/UserCard.jsx
+++ src/Components/UserCard.jsx
@@ -5,14 +5,18 @@ import './UserCard.css';
 const UserCard = ({ isLinked, user: { username, name, avatar_url } }) => {
   return (
     <div className="usercard">
-      <img src={avatar_url} alt={`avatar for ${username}`} />
       {isLinked ? (
         <Link to={`/users/${username}`}>
+          <img src={avatar_url} alt={`avatar for ${username}`} />
           <h2>{username}</h2>
         </Link>
       ) : (
-        <h2>{username}</h2>
+        <>
+          <img src={avatar_url} alt={`avatar for ${username}`} />
+          <h2>{username}</h2>
+        </>
       )}
+
       <h2>{name}</h2>
     </div>
   );

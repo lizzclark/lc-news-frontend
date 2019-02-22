7cbc7d2ef3b36001af5fb5436537148ed70ae98e
lizzclark <erclark13@outlook.com>
2019-02-22 08:57:47 +0000

    add links to Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,9 +5,18 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p className="footer-text">
-        by <a href="#">Lizz Clark</a> | <a href="">LC News on Github</a> |{' '}
-        <a href="#">Backend news API on Github</a> |{' '}
-        <a href="#">Backend news API on Heroku</a>
+        by <a href="https://github.com/lizzclark">Lizz Clark</a> |{' '}
+        <a href="https://github.com/lizzclark/lc-news-frontend">
+          LC News on Github
+        </a>{' '}
+        |{' '}
+        <a href="https://github.com/lizzclark/lc-news">
+          Backend news API on Github
+        </a>{' '}
+        |{' '}
+        <a href="https://lc-news.herokuapp.com/api">
+          Backend news API on Heroku
+        </a>
       </p>
     </footer>
   );
